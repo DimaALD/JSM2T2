5c8f81173d2effa33a35c17c65fb40ef8cf4e61b
Dzmitry_Aldoshin <Dzmitry_Aldoshin@epam.com>
2018-11-23 20:05:10 +0300

    t2

---
+++ JSM2T21/Task2.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const wstream = fs.createWriteStream('STAR_WARS.txt')
+const request = require('request-promise')
+const yargs = require('yargs')
+
+// eslint-disable-next-line no-unused-expressions
+yargs.command(
+  'find',
+  'Input type and name to find info about Star Wars universe.',
+  {},
+  (argv) => {
+    findInfo(argv)
+  })
+  .demandCommand(1, 'You need at least one command before moving on').argv
+
+function findInfo (argv) {
+  const options = {
+    method: 'GET',
+    uri: 'https://swapi.co/api/' + `${argv.type}` + '/?search=' + `${argv.name}`,
+    json: true
+  }
+  request(options).then(body => {
+    printInFile(JSON.stringify(body.results, null, ' '))
+  }).catch(err => { console.log(err.message) })
+}
+
+function printInFile (json) {
+  wstream.write(json)
+  wstream.end()
+}
